e6fbf619a5db98b62863e6a30d656f1694db350d
Jani Kekäläinen <92083120+Developer250@users.noreply.github.com>
2022-10-03 14:33:42 +0300

    Update lomake.js

--- Java-projektit/projekti1/lomake.js
+++ Java-projektit/projekti1/lomake.js
@@ -1,6 +1,49 @@
-function vaihda() //väliaikainen function ja (vaihda) nimi pitää korjata oikeaksi
+function projekti() //väliaikainen function ja (vaihda) nimi pitää korjata oikeaksi
 {
-  var etunimi =
-  var sukunimi =
-  var sposti =
+  var etunimi = form.enimi.value;
+  var sukunimi = form.sukunimi.value;
+  var sposti = form.email.value;
+  var pallo = form.pallo.value;
+  var palaute = form.palaute.value;
+  var check = form.boksi;
+
+  //Tarkastetaan etunimen pituus
+  if (etunimi.length < 6) {
+    alert("Anna vähintään 6-merkkinen etunimi");
+    form.enimi.focus();
+    return false; //Ei palauta mitään
+  }
+  
+  var tarkistus = /\S+@\S+/;
+  if (!tarkistus.test(sahkoposti)) {
+    alert("Anna kunnon sähköposti");
+  }
+  
+  //Tarkastetaan tekstialue
+  if (palaute.length < 30) {
+    alert("Palautteessa tulee olla vähintään 30-merkkiä!");
+    form.palaute.focus();
+    return false;
+  }
+  
+  //Tarkastetaan radio-buttonit
+  //Tehdään tosi/epätosi -muuttuja, joka asetetaan sitten epätodeksi
+  var vastaus = false;
+  //Edellistä kohtaa varten tehdään silmukka, joka sitten käy jokaisen painikkeen läpi
+  for (var i = 0; i < pallo.length; i++) {
+    if (pallo[i].checked == true) {
+      vastaus = true;
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
 }
