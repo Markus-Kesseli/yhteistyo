f1675fb5611c6b75a36e8fbcff36f33f358c5afd
Jani Kekäläinen <92083120+Developer250@users.noreply.github.com>
2022-10-10 19:20:10 +0300

    Update arvauspeli.js

--- Java-projektit/Projekti 2/arvauspeli.js
+++ Java-projektit/Projekti 2/arvauspeli.js
@@ -1,34 +1,30 @@
-var msg1 = document.getElementById("message1");
-var msg2 = document.getElementById("message2");
-var msg3 = document.getElementById("message3");
+  var message1 = document.getElementById("message1");
+  var message2 = document.getElementById("message2");
+  var message3 = document.getElementById("message3");
 
-var answer = Math.floor(Math.random()*10) + 1;
-var nro_of_guesses = 0;
-var guessed_nums = [];
+  var answer = Math.floor(Math.random()*10) + 1;
+  var nro_of_guesses = 0;
+  var guessed_nums = [];
 
-function arvauspeli() {
-  var user_guess = document.getElementById("guess").value;
-  if (user_guess < 1 || user_guess > 10) {
+    function arvauspeli() {
+     var user_guess = document.getElementById("guess").value;
+    if (user_guess < 1 || user_guess > 10) {
     alert("Ole hyvä ja laita 1-10 numeroiden väliltä");
-  }
-  else {
-    guessed_nums.push(user_guess);
-    nro_of_guesses += 1;
-    if (user_guess < answer) {
-      msg1.textContent = "Your guess is too low";
-      msg2.textContent = "Nro. Of guesses: " + nro_of_guesses;
-      msg3.textContent = "Guessed: numbers are: :" + guessed_nums;
-      }
-      else if (user_guess > answer) {
-        msg1.textContent = "Your guess is too high";
-        msg2.textContent = "Nro. of guesses: " + nro_of_guesses;
-        msg3.textContent = "Guessed numbers are: " + guessed_nums;
-      }
-        else if (user_guess == answer) {
-          msg1.textContent = "Nice! You win!";
-          msg2.textContent = "The number was: " + answer;
-          msg3.textContent = "You guessed it in:" + nro_of_guesses + "guesses";
-          document.getElementById("btn").disabled = true;
-        }
-      }
     }
+    else {
+      guessed_nums.push(user_guess);
+      }
+    if (user_guess < answer) {
+      message1.innerHTML = "Your guess is too low.";
+      }
+     else if (user_guess > answer) {
+        message1.innerHTML= "Your guess is too high.";
+      }
+     else {
+       message1.innerHTML = "Nice! You win!";
+     }
+      nro_of_guesses++;
+      message2.innerHTML = "nro_of_guesses: " + nro_of_guesses;
+      message2.innerHTML = "guessed nums: " + guessed_nums;
+    }
+
