3d553f92ca1f3a79f071be1bd5f1618b0228466c
Jani Kekäläinen <92083120+Developer250@users.noreply.github.com>
2022-10-10 22:07:10 +0300

    Update arvauspeli.js

--- Java-projektit/Projekti 2/arvauspeli.js
+++ Java-projektit/Projekti 2/arvauspeli.js
@@ -1,30 +1,39 @@
-  var message1 = document.getElementById("message1");
-  var message2 = document.getElementById("message2");
-  var message3 = document.getElementById("message3");
+  var luku1 = document.getElementById("message1");
+  var luku2= document.getElementById("message2");
+  var luku3 = document.getElementById("message3");
 
   var answer = Math.floor(Math.random()*10) + 1;
-  var nro_of_guesses = 0;
+  var nro_of_guesses = 1;
   var guessed_nums = [];
 
-    function arvauspeli() {
-     var user_guess = document.getElementById("guess").value;
-    if (user_guess < 1 || user_guess > 10) {
-    alert("Ole hyvä ja laita 1-10 numeroiden väliltä");
+  function arvauspeli()
+{
+  var user_guess = document.getElementById("guess").value;
+   if (user_guess >= 3) {
+    guessed_nums.push(user_guess);
+    document.getElementById("btn").hidden = true;
+    message2.innerHTML = "nro_of_guesses: " + nro_of_guesses;
+    message3.innerHTML = "guessed nums: " + guessed_nums;
+  }
+    else {
+    if
+      (user_guess < 1 || user_guess > 10) {
+      alert("Ole hyvä ja anna numero 1 ja 10 välillä")
+      }
+    else {
+     guessed_nums.push(user_guess);
+    }
+    if (user_guess < answer) {
+     message1.innerHTML = "Your guess is too low.";
+    }
+    else if (user_guess > answer) {
+      message1.innerHTML = "Your guess is too high.";
     }
     else {
-      guessed_nums.push(user_guess);
+      message1.innerHTML = "Nice! You win!";
       }
-    if (user_guess < answer) {
-      message1.innerHTML = "Your guess is too low.";
-      }
-     else if (user_guess > answer) {
-        message1.innerHTML= "Your guess is too high.";
-      }
-     else {
-       message1.innerHTML = "Nice! You win!";
-     }
-      nro_of_guesses++;
       message2.innerHTML = "nro_of_guesses: " + nro_of_guesses;
-      message2.innerHTML = "guessed nums: " + guessed_nums;
-  }
-
+      message3.innerHTML = "guessed nums: " + guessed_nums;
+    }
+      nro_of_guesses++;
+}
