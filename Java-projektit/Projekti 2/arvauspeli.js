248a313bc33147abc375c553d9bcc55d5c3f1c53
Jani Kekäläinen <92083120+Developer250@users.noreply.github.com>
2022-10-10 13:09:35 +0300

    Update arvauspeli.js

--- Java-projektit/Projekti 2/arvauspeli.js
+++ Java-projektit/Projekti 2/arvauspeli.js
@@ -3,8 +3,8 @@ var msg2 = document.getElementById("message2");
 var msg3 = document.getElementById("message3");
 
 var answer = Math.floor(Math.random()*10) + 1;
-var Nro_of_Guesses = 0;
-var Guessed_nums = [];
+var nro_of_guesses = 0;
+var guessed_nums = [];
 
 function arvauspeli() {
   var user_guess = document.getElementById("arvaa").value;
@@ -13,21 +13,21 @@ function arvauspeli() {
   }
   else {
     guessed_nums.push(user_guess);
-    Nro_of_Guesses += 1;
+    nro_of_geuesses += 1;
     if (user_guess < answer) {
       msg1.textContent = "Your guess is too low";
-      msg2.textContent = "Nro. Of guesses: " + Nro_of_guesses;
+      msg2.textContent = "Nro. Of guesses: " + nro_of_guesses;
       msg3.textContent = "Guessed: numbers are: :" + guessed_nums;
       }
       else if (user_guess > answer) {
         msg1.textContent = "Your guess is too high";
-        msg2.textContent = "Nro. Of guesses: " + Nro_of_guesses;
+        msg2.textContent = "Nro. Of guesses: " + nro_of_guesses;
         msg3.textContent = "Guessed: numbers are: :" + guessed_nums;
       }
         else if (user_guess == answer) {
          msg1.textContent = "Nice! You win!";
          msg2.textContent = "The number was: " + answer;
-         msg3.textContent = "Guessed: numbers are: :" + Nro_of_guesses + "guesses" ;
+         msg3.textContent = "Guessed: numbers are: :" + nro_of_guesses + "guesses" ;
         }
       }
     }
