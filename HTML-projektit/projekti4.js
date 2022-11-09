87992e515759e4be77c5742c6ec5f65c44fbe6b1
Developer250 <jani.kekalainen@hotmail.com>
2022-11-09 17:32:06 +0200

    j

--- HTML-projektit/projekti4.js
+++ HTML-projektit/projekti4.js
@@ -1,22 +1,21 @@
-function projekti()
-{
+  function ilmoittautuminen()  {
   //Luodaan muuttujat
   var etunimi = document.formi.enimi.value;
   var sukunimi = document.formi.snimi.value;
-  var check = document.formi.mp;
-  var checkVastaus = document.formi.kivointa;
-  var pallo = document.formi.tietokone;
   //Lisää tarkastukset myös allaoleviin muuttujiin
-  var osoitetitedot = document.formi.osoitetitedot;
-  var puh = document.formi.puh;
+  var osoitetitedot = document.formi.osoitetitedot.value;
+  var puh = document.formi.puh.value;
   var sposti = document.formi.email.value;
-  var oppilaitos = document.formi.oppilaitos;
-  var opiskelija = document.formi.opiskelija;
-  var syntymaaika = document.formi.syntymaaika;
-  var hinta = document.formi.hinta;
-  var erityisruokavalio = document.formi.ruokvalio;
+  var oppilaitos = document.formi.oppilaitos.value;
+  var opiskelija = document.formi.opiskelija.value;
+  var syntymaaika = document.formi.syntymaaika.value;
+  var hinta = document.formi.hinta.value;
+  var erityisruokavalio = document.formi.erityisruokavalio.value;
   var lisatieto = document.formi.lisatieto.value;
-  
+  var checkVastaus = document.formi.kivointa;
+
+
+
   //Tarkastetaan etunimen pituus
   if (etunimi.length < 4)
   {
@@ -35,38 +34,44 @@ function projekti()
   if (!tarkistus.test(sposti)) {
     alert("Anna kunnon sähköposti");
   }
+  //puhelinnumeron tarkistus
 
-  if (salasana.length < 6)
+  var puh = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im
+  if(!puh.test(puh))
   {
-      alert("Anna vähintään 6-merkkinen salasana");
-      return false;
+     alert("Anna  kunnon puhelinnumero!");
+  }
 
-  //Tarkastetaan radio-buttonit
-  //Tehdään tosi/epätosi -muuttuja, joka asetetaan sitten epätodeksi
-  var vastaus = false;
-  //Edellistä kohtaa varten tehdään silmukka, joka sitten käy jokaisen painikkeen läpi
-  for (var i = 0; i < pallo.length; i++) {
-    if (pallo[i].checked == true) {
-      vastaus = true;
-    }
+  if (oppilaitos.length < 15)
+  {
+    alert("Anna vähintään 15-merkkinen oppilaitos");
+    return false; //Ei palauta mitään
   }
-  //Tarkastetaan checkboxit
-  var tarkastaVastaus = false;
-  for (var h = 0; h < check.length; h++) {
-    if (check[h])checked == true;
-    {
-      checkVastaus = true;
-    }
+
+  if (opiskelija.length < 10)
+  {
+    alert("Anna vähintään 10-merkkinen opiskelija");
+    return false; //Ei palauta mitään
   }
-}
+//Meni syntymäajan tarkistuksen läpi, vaikka väärin laittaisi. Pitää kokeilla toista metodia
+  var now = new Date();
+  //Validate birth date some time before today's date and
+  //within 120 years
+  if(syntymaaika == null ||
+    (syntymaaika < now && now.getFullYear() - date.getFullYear() < 120)){
+   true;
+  } else{
+   false;
+   }
+
 //Tarkastetaan tekstialue
-if (palaute.length < 20) {
-  alert("Palautteessa tulee olla vähintään 20-merkkiä!");
-  document.formi.palautet.focus();
+if (lisatieto.length < 20) {
+  alert("Lisätiedossa tulee olla vähintään 20-merkkiä!");
+  document.formi.lisatieto.focus();
   return false;
 }
   if (checkVastaus == false) {
-    alert("Et valinnut, mikä on kivaa");
+    alert("Anna lisätieto");
     return false;
   }
   else {
