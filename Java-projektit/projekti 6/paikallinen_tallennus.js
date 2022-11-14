0f0d2d04d76e118907be5ce6c9e91b6ed02862f1
Markus Kesseli <markus.kesseli@gmail.com>
2022-11-14 11:00:52 +0200

    projekti

--- Java-projektit/projekti 6/paikallinen_tallennus.js
+++ Java-projektit/projekti 6/paikallinen_tallennus.js
@@ -1,5 +1,5 @@
 
-  let lomake1B = () => {
+  function lomake1B() {
 
   //Haetaan html-documentista käyttäjän syöttämät arvot
   const etunimi = document.form.enimi;
@@ -10,94 +10,14 @@
   const puhe = document.form.puh;
   const sposti = document.form.email;
 
-  //Tarkistetaan nimi
-  if (etunimi.length < 4)
-  {
-    alert("Anna vähintään 4-merkkinen etunimi!");
-    return false; //Ei palauta mitään
-  }
-
-  //Tarkastetaan sukunimen pituus
-  if (sukunimi.length < 5)
-  {
-    alert("Anna vähintään 5-merkkinen sukunimi!");
-    return false; //Ei palauta mitään
-  }
-
-  //Tarkistetaan lähiosoite
-  if (lahiosoite.length < 5)
-  {
-    alert("Anna lähiosoite!");
-    return false; //Ei palauta mitään
-  }
-
-  //Tarkistetaan postinumero
-  if (postinumero.length < 5)
-  {
-    alert("Anna postinumero!");
-    return false; //Ei palauta mitään
-  }
-
-  //Tarkistetaan postitoimipaikka
-  if (postitoimipaikka.length < 4)
-  {
-    alert("Anna postitoimipaikka!");
-    return false; //Ei palauta mitään
-  }
-
-  //puhelinnumeron tarkistus
-  var puh = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4}$/im
-  if(puh.test(puhe))
-  {
-   alert("Anna puhelinnumero!");
-   return false;
-  }
-
-  //Tarkastetaan sähköposti
-  var tarkistus = /\S+@\S+/;
-  if (tarkistus.test(sposti)) {
-  alert("Anna sähköposti!");
-  }
-
-  var data = "Nimi: " + etunimi.value + "\r\n" +
-  "Sukunimi: " + sukunimi.value + "\r\n" +
-  "Lähiosoite: " + lahiosoite.value + "\r\n" +
-  "Postinumero: " + postinumero.value + "\r\n" +
-  "Postitoimipaikka: " + postitoimipaikka.value + "\r\n" +
-  "Puhelin: " + puhe.value + "\r\n" +
-  "Sähköposti: " + sposti.value;
-
-  console.log(data);
-
-  const textToBLOB = new Blob([data], { type: "text/plain" });
-
-  var tiedosto = new Date();
-
-            var month =new Date(); //months from 1-12
-            month = month.getMonth();
-
-            var day = new Date();
-            var day = day.getUTCDate();
-
-            var year = new Date();
-            var year = year.getUTCFullYear();
-
-            newdate = year + "/" + month + "/" + day;
-            const sFileName = tiedosto; // The file to save the data.
-
-            let newLink = document.createElement("a");
-            newLink.download = new Date();
-
-            if (window.webkitURL != null) {
-                newLink.href = window.webkitURL.createObjectURL(textToBLOB);
-            } else {
-                newLink.href = window.URL.createObjectURL(textToBLOB);
-                newLink.style.display = "none";
-                document.body.appendChild(newLink);
-            }
-
-            newLink.click();
-        };
+  localStorage.setItem("Etunimi", etunimi.value);
+  localStorage.setItem("Sukunimi", sukunimi.value);
+  localStorage.setItem("Lähiosoite", lahiosoite.value);
+  localStorage.setItem("Postinumero", postinumero.value);
+  localStorage.setItem("Postitoimipaikka", postitoimipaikka.value);
+  localStorage.setItem("Puhelin", puhe.value);
+  localStorage.setItem("Sähköposti", sposti.value);
+};
 
 
   function tyhjenna()
@@ -106,4 +26,9 @@
     {
       document.getElementById('clear').reset();
     }
+}
+
+function poista()
+{
+  localStorage.clear();
 }
