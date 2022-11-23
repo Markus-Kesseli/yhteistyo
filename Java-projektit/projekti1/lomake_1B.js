45ff3572696f1898d4645a8c7bc352279fd1ebd2
Markus Kesseli <markus.kesseli@gmail.com>
2022-11-23 09:59:43 +0200

    projekti

--- Java-projektit/projekti1/lomake_1B.js
+++ Java-projektit/projekti1/lomake_1B.js
@@ -3,13 +3,17 @@ function lomake1B() {
   var form = document.getElementById("kyselylomake").value;
   var nimi = document.getElementById("nimisyotto").value;
   var sposti = document.getElementById("emailsyotto").value;
-  var ika = document.getElementById("ikasyotto").value;
-  var palaute = document.form.palautet.value;
+  var ika = document.getElementById("ikasyotot").value;
+  var palaute = document.getElementById("palautet").value;
+  var ruo1 = document.getElementById('ruoka1');
+  var ruo2 = document.getElementById('ruoka2');
+  var ruo3 = document.getElementById('ruoka3');
 
   //Tarkistetaan nimi
-  for (var i = 0; i < nimi.length; i++) {
-    alert("Et syöttänyt koko nimeä");
-    return false;
+  if (nimi.length < 5)
+  {
+    alert("Anna nimi");
+    return false; //Ei palauta mitään
   }
   //min- ja max-määrite?
   var min = (Math.min(1));
@@ -20,20 +24,33 @@ function lomake1B() {
     alert("Syötä ikäsi");
     return false;
   }
-  //Tarkastetaan s-posti
+  //Tarkastetaan sähköposti
   var tarkistus = /\S+@\S+/;
   if (!tarkistus.test(sposti)) {
-  alert("Anna kunnon sähköposti");
-  return false;
-  }
-}
-  if (palaute.length < 35) {
-    alert("Palautteessa tulee olla vähintään 35-merkkiä!");
+    alert("Anna oikea sähköposti!");
   }
 
- var vastaus = false;
- //Edellistä kohtaa varten tehdään silmukka, joka sitten käy jokaisen painikkeen läpi
-  var ruokalaji = ruoka, ruoka, ruoka;
-  if (ruokalaji == true) {
-  vastaus = true;
+
+
+  if(ruo1.checked)
+  {
+
   }
+  else if (ruo2.checked)
+  {
+
+  }
+  else if (ruo3.checked)
+  {
+
+  }
+  else {
+    alert("Et valinnut lempiruokaasi!")
+  };
+
+  if (palaute.length < 35)
+  {
+    alert("Palautteessa tulee olla vähintään 35-merkkiä!");
+  };
+
+}
