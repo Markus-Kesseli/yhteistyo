57ead3789e33af208333e7e47a50355502bc3444
Developer250 <jani.kekalainen@hotmail.com>
2022-11-23 16:45:55 +0200

    Projekti 8

---
+++ Java-projektit/projekti 8/scrum.js
@@ -0,0 +1,9 @@
+const list_items = document.querySelectorAll('.list-item');
+const lists = document.querySelectorAll('.list');
+
+let draggedItem = null;
+
+for (let i = 0; i < list_items.length; i++) {
+	const item = list_items[i];
+
+	
