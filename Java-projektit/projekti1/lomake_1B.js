02c49fb950806087f7746d7e1526b3ec71794669
Markus Kesseli <markus.kesseli@gmail.com>
2022-11-23 10:35:25 +0200

    projekti

--- Java-projektit/projekti1/lomake_1B.js
+++ Java-projektit/projekti1/lomake_1B.js
@@ -8,6 +8,7 @@ function lomake1B() {
   var ruo1 = document.getElementById('ruoka1');
   var ruo2 = document.getElementById('ruoka2');
   var ruo3 = document.getElementById('ruoka3');
+  var valinta = document.getElementById('pudotus')
 
   //Tarkistetaan nimi
   if (nimi.length < 5)
@@ -15,42 +16,46 @@ function lomake1B() {
     alert("Anna nimi");
     return false; //Ei palauta mitään
   }
-  //min- ja max-määrite?
-  var min = (Math.min(1));
-  var max = (Math.max(110));
 
-  //Tarkastetaan ikä
-  for (var i = 0; i < ika; i++) {
-    alert("Syötä ikäsi");
-    return false;
-  }
+  if ( ika < 1 || ika>100 ) {
+      alert("The age must be a number between 1 and 100");
+       return false;
+   }
+  return true;
+
   //Tarkastetaan sähköposti
   var tarkistus = /\S+@\S+/;
   if (!tarkistus.test(sposti)) {
     alert("Anna oikea sähköposti!");
   }
 
+  if(valinta.selectedIndex <=0)
+      {
+        alert("Valitse ajoneuvo!");
+      }
 
+      if(ruo1.checked)
+      {
 
-  if(ruo1.checked)
-  {
+      }
+      else if (ruo2.checked)
+      {
 
-  }
-  else if (ruo2.checked)
-  {
+      }
+      else if (ruo3.checked)
+      {
 
-  }
-  else if (ruo3.checked)
-  {
+      }
+      else {
+        alert("Et valinnut lempiruokaasi!")
+      };
 
-  }
-  else {
-    alert("Et valinnut lempiruokaasi!")
-  };
+      if (palaute.length < 20)
+      {
+        alert("Palautteessa tulee olla vähintään 20-merkkiä!")
+      }
+      else {
+        alert("Kiitos lomakkeen täytöstä!")
+      }
 
-  if (palaute.length < 35)
-  {
-    alert("Palautteessa tulee olla vähintään 35-merkkiä!");
-  };
-
-}
+   }
