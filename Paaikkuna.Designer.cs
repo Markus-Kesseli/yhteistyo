6bf00f39b8a0499d0c5f988a4ce9cd46543b61c1
Jani Kekäläinen <92083120+Developer250@users.noreply.github.com>
2023-03-08 12:35:56 +0200

    Add files via upload

---
+++ Paaikkuna.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace Hotelli
+{
+    partial class Paaikkuna
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.asiakkaidenHallintaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.hallitseVarauksiaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.hAllitseHuoneitaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.asiakkaidenHallintaToolStripMenuItem,
+            this.hallitseVarauksiaToolStripMenuItem,
+            this.hAllitseHuoneitaToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(730, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // asiakkaidenHallintaToolStripMenuItem
+            // 
+            this.asiakkaidenHallintaToolStripMenuItem.Name = "asiakkaidenHallintaToolStripMenuItem";
+            this.asiakkaidenHallintaToolStripMenuItem.Size = new System.Drawing.Size(149, 24);
+            this.asiakkaidenHallintaToolStripMenuItem.Text = "Hallitse asiakkaikta";
+            this.asiakkaidenHallintaToolStripMenuItem.Click += new System.EventHandler(this.asiakkaidenHallintaToolStripMenuItem_Click);
+            // 
+            // hallitseVarauksiaToolStripMenuItem
+            // 
+            this.hallitseVarauksiaToolStripMenuItem.Name = "hallitseVarauksiaToolStripMenuItem";
+            this.hallitseVarauksiaToolStripMenuItem.Size = new System.Drawing.Size(138, 24);
+            this.hallitseVarauksiaToolStripMenuItem.Text = "Hallitse varauksia";
+            this.hallitseVarauksiaToolStripMenuItem.Click += new System.EventHandler(this.hallitseVarauksiaToolStripMenuItem_Click);
+            // 
+            // hAllitseHuoneitaToolStripMenuItem
+            // 
+            this.hAllitseHuoneitaToolStripMenuItem.Name = "hAllitseHuoneitaToolStripMenuItem";
+            this.hAllitseHuoneitaToolStripMenuItem.Size = new System.Drawing.Size(135, 24);
+            this.hAllitseHuoneitaToolStripMenuItem.Text = "Hallitse huoneita";
+            this.hAllitseHuoneitaToolStripMenuItem.Click += new System.EventHandler(this.hAllitseHuoneitaToolStripMenuItem_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Hotelli.Properties.Resources.hotellihuone;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 31);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1116, 667);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Paaikkuna
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(730, 512);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Paaikkuna";
+            this.Text = "Paaikkuna";
+            this.Load += new System.EventHandler(this.Paaikkuna_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem asiakkaidenHallintaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem hallitseVarauksiaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem hAllitseHuoneitaToolStripMenuItem;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
