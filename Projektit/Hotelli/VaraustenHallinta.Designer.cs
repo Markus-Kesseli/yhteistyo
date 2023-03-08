e461b869c3a05d08671e0a35184475bd68ed582b
Markus Kesseli <markus.kesseli@gmail.com>
2023-03-08 14:48:02 +0200

    6.0

---
+++ Projektit/Hotelli/VaraustenHallinta.Designer.cs
@@ -0,0 +1,241 @@
+﻿namespace Hotelli
+{
+    partial class VaraustenHallinta
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.VarausLisaaBT = new System.Windows.Forms.Button();
+            this.VarausMuokkaaBT = new System.Windows.Forms.Button();
+            this.VarausTallennaBT = new System.Windows.Forms.Button();
+            this.VarausTyhjennaBT = new System.Windows.Forms.Button();
+            this.varausnroTB = new System.Windows.Forms.TextBox();
+            this.asiakasnroTB = new System.Windows.Forms.TextBox();
+            this.huonetyyppiTB = new System.Windows.Forms.TextBox();
+            this.huonenroTB = new System.Windows.Forms.TextBox();
+            this.sisaanTB = new System.Windows.Forms.TextBox();
+            this.ulosTB = new System.Windows.Forms.TextBox();
+            this.VaraustenHallintaDG = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.VaraustenHallintaDG)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(37, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Varaus nro:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(37, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Asiakas nro:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(37, 93);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Huonetyyppi:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(37, 120);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(72, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Huoneen nro:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(37, 147);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(42, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Sisään:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(37, 174);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(31, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Ulos:";
+            // 
+            // VarausLisaaBT
+            // 
+            this.VarausLisaaBT.Location = new System.Drawing.Point(40, 234);
+            this.VarausLisaaBT.Name = "VarausLisaaBT";
+            this.VarausLisaaBT.Size = new System.Drawing.Size(75, 23);
+            this.VarausLisaaBT.TabIndex = 6;
+            this.VarausLisaaBT.Text = "Lisää";
+            this.VarausLisaaBT.UseVisualStyleBackColor = true;
+            // 
+            // VarausMuokkaaBT
+            // 
+            this.VarausMuokkaaBT.Location = new System.Drawing.Point(121, 234);
+            this.VarausMuokkaaBT.Name = "VarausMuokkaaBT";
+            this.VarausMuokkaaBT.Size = new System.Drawing.Size(75, 23);
+            this.VarausMuokkaaBT.TabIndex = 7;
+            this.VarausMuokkaaBT.Text = "Muokkaa";
+            this.VarausMuokkaaBT.UseVisualStyleBackColor = true;
+            // 
+            // VarausTallennaBT
+            // 
+            this.VarausTallennaBT.Location = new System.Drawing.Point(40, 263);
+            this.VarausTallennaBT.Name = "VarausTallennaBT";
+            this.VarausTallennaBT.Size = new System.Drawing.Size(75, 23);
+            this.VarausTallennaBT.TabIndex = 8;
+            this.VarausTallennaBT.Text = "Tallenna";
+            this.VarausTallennaBT.UseVisualStyleBackColor = true;
+            // 
+            // VarausTyhjennaBT
+            // 
+            this.VarausTyhjennaBT.Location = new System.Drawing.Point(121, 263);
+            this.VarausTyhjennaBT.Name = "VarausTyhjennaBT";
+            this.VarausTyhjennaBT.Size = new System.Drawing.Size(75, 23);
+            this.VarausTyhjennaBT.TabIndex = 9;
+            this.VarausTyhjennaBT.Text = "Tyhjennä";
+            this.VarausTyhjennaBT.UseVisualStyleBackColor = true;
+            // 
+            // varausnroTB
+            // 
+            this.varausnroTB.Location = new System.Drawing.Point(121, 36);
+            this.varausnroTB.Name = "varausnroTB";
+            this.varausnroTB.Size = new System.Drawing.Size(125, 20);
+            this.varausnroTB.TabIndex = 10;
+            // 
+            // asiakasnroTB
+            // 
+            this.asiakasnroTB.Location = new System.Drawing.Point(121, 63);
+            this.asiakasnroTB.Name = "asiakasnroTB";
+            this.asiakasnroTB.Size = new System.Drawing.Size(125, 20);
+            this.asiakasnroTB.TabIndex = 11;
+            // 
+            // huonetyyppiTB
+            // 
+            this.huonetyyppiTB.Location = new System.Drawing.Point(121, 90);
+            this.huonetyyppiTB.Name = "huonetyyppiTB";
+            this.huonetyyppiTB.Size = new System.Drawing.Size(125, 20);
+            this.huonetyyppiTB.TabIndex = 12;
+            // 
+            // huonenroTB
+            // 
+            this.huonenroTB.Location = new System.Drawing.Point(121, 117);
+            this.huonenroTB.Name = "huonenroTB";
+            this.huonenroTB.Size = new System.Drawing.Size(125, 20);
+            this.huonenroTB.TabIndex = 13;
+            // 
+            // sisaanTB
+            // 
+            this.sisaanTB.Location = new System.Drawing.Point(121, 144);
+            this.sisaanTB.Name = "sisaanTB";
+            this.sisaanTB.Size = new System.Drawing.Size(125, 20);
+            this.sisaanTB.TabIndex = 14;
+            // 
+            // ulosTB
+            // 
+            this.ulosTB.Location = new System.Drawing.Point(121, 171);
+            this.ulosTB.Name = "ulosTB";
+            this.ulosTB.Size = new System.Drawing.Size(125, 20);
+            this.ulosTB.TabIndex = 15;
+            // 
+            // VaraustenHallintaDG
+            // 
+            this.VaraustenHallintaDG.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.VaraustenHallintaDG.Location = new System.Drawing.Point(286, 12);
+            this.VaraustenHallintaDG.Name = "VaraustenHallintaDG";
+            this.VaraustenHallintaDG.Size = new System.Drawing.Size(502, 274);
+            this.VaraustenHallintaDG.TabIndex = 16;
+            // 
+            // VaraustenHallinta
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 298);
+            this.Controls.Add(this.VaraustenHallintaDG);
+            this.Controls.Add(this.ulosTB);
+            this.Controls.Add(this.sisaanTB);
+            this.Controls.Add(this.huonenroTB);
+            this.Controls.Add(this.huonetyyppiTB);
+            this.Controls.Add(this.asiakasnroTB);
+            this.Controls.Add(this.varausnroTB);
+            this.Controls.Add(this.VarausTyhjennaBT);
+            this.Controls.Add(this.VarausTallennaBT);
+            this.Controls.Add(this.VarausMuokkaaBT);
+            this.Controls.Add(this.VarausLisaaBT);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "VaraustenHallinta";
+            this.Text = "VaraustenHallinta";
+            ((System.ComponentModel.ISupportInitialize)(this.VaraustenHallintaDG)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button VarausLisaaBT;
+        private System.Windows.Forms.Button VarausMuokkaaBT;
+        private System.Windows.Forms.Button VarausTallennaBT;
+        private System.Windows.Forms.Button VarausTyhjennaBT;
+        private System.Windows.Forms.TextBox varausnroTB;
+        private System.Windows.Forms.TextBox asiakasnroTB;
+        private System.Windows.Forms.TextBox huonetyyppiTB;
+        private System.Windows.Forms.TextBox huonenroTB;
+        private System.Windows.Forms.TextBox sisaanTB;
+        private System.Windows.Forms.TextBox ulosTB;
+        private System.Windows.Forms.DataGridView VaraustenHallintaDG;
+    }
+}
