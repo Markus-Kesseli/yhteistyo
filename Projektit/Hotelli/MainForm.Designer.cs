2a0753a797d3bf53ed659348fc85a295b752e5e2
Developer250 <jani.kekalainen@hotmail.com>
2023-04-03 13:50:09 +0300

    korjaa virheet

---
+++ Projektit/Hotelli/MainForm.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace Hotelli
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.manageCLientsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.manageRoomsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.manageClientsToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.manageCLientsToolStripMenuItem,
+            this.manageRoomsToolStripMenuItem,
+            this.manageClientsToolStripMenuItem1});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // manageCLientsToolStripMenuItem
+            // 
+            this.manageCLientsToolStripMenuItem.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.manageCLientsToolStripMenuItem.Name = "manageCLientsToolStripMenuItem";
+            this.manageCLientsToolStripMenuItem.Size = new System.Drawing.Size(124, 24);
+            this.manageCLientsToolStripMenuItem.Text = "ManageCLients";
+            // 
+            // manageRoomsToolStripMenuItem
+            // 
+            this.manageRoomsToolStripMenuItem.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.manageRoomsToolStripMenuItem.Name = "manageRoomsToolStripMenuItem";
+            this.manageRoomsToolStripMenuItem.Size = new System.Drawing.Size(123, 24);
+            this.manageRoomsToolStripMenuItem.Text = "ManageRooms";
+            // 
+            // manageClientsToolStripMenuItem1
+            // 
+            this.manageClientsToolStripMenuItem1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.manageClientsToolStripMenuItem1.Name = "manageClientsToolStripMenuItem1";
+            this.manageClientsToolStripMenuItem1.Size = new System.Drawing.Size(121, 24);
+            this.manageClientsToolStripMenuItem1.Text = "ManageClients";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MediumVioletRed;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "MainForm";
+            this.Text = "MainForm";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem manageCLientsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem manageRoomsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem manageClientsToolStripMenuItem1;
+    }
+}
---
+++ Projektit/Hotelli/MainForm.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hotel_System
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            initializeComponent();
+        }
+
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void manageClientsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ManageClientsForm manageCF = new ManageClientsForm();
+            manageCF.ShowDialog();
+        }
+
+        private void manageRoomsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ManageRoomsForm1 manageRF = new ManageRoomsForm1();
+            manageRF.ShowDialog();
+        }
+
+        private void manageReservationsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ManageReservationsForm1 manageResF = new ManageReservationsForm1();
+            manageResF.ShowDialog();
+        }
+    }
+}
